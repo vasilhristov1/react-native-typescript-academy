3868ef88a71726518e9e096d149e362eef20fef2
Vasil Hristov <vasil.hristov@Vasils-Mac-mini.local>
2022-08-15 16:23:29 +0300

    08-ts-react-advanced added

---
+++ 08-ts-react-advanced/src/TodoList.tsx
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+import { Todo, TodoStatus } from "./todo.model";
+import { FilterType, TodoListener } from "./TodoApp";
+import TodoItem from "./TodoItem";
+import './TodoList.css'
+
+interface Props {
+    todos: Todo[];
+    filter: FilterType;
+    onUpdate: TodoListener;
+    onDelete: TodoListener;
+    onEdit: TodoListener;
+}
+
+export default function TodoList({ todos, filter, ...rest }: Props) {
+    const visibleTodos = (todos: Todo[], filter: FilterType) => todos.filter(todo => !filter ? true : todo.status === filter);
+    const memizedVisibleTodos = useMemo(() => visibleTodos(todos, filter), [todos, filter]);
+    return (<div className="TodoList">
+        {
+            memizedVisibleTodos.map(todo =>
+                <TodoItem todo={todo} key={todo.id} {...rest} />)
+        }
+    </div>)
+}
